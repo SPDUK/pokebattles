54270284a6fd43bbb4482bfa388cbc203a0acb7e
Steve P <spdevuk@gmail.com>
2019-10-26 13:08:37 +0100

    feat: add links to about page, remove footer

--- src/App.vue
+++ src/App.vue
@@ -43,9 +43,6 @@
         </v-row>
       </v-container>
     </v-content>
-    <v-footer app>
-      <span>&copy; 2019</span>
-    </v-footer>
   </v-app>
 </template>
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    Github:
+    <a href="https://github.com/SPDUK/pokebattles"
+      >https://github.com/SPDUK/pokebattles</a
+    >
+
+    <br />
+    Created using: <a href="https://vuejs.org/">Vue.js</a>,
+    <a href="https://vuetifyjs.com/en/">Vuetify</a>,
+    <a href="https://pokeapi.co/">PokeAPI</a>,
+    <a href="https://wlada.github.io/vue-carousel-3d/">Vue Carousel 3d</a>
   </div>
 </template>
+
+<style>
+.about {
+  text-align: left;
+  margin: 0 auto;
+  max-width: 500px;
+}
+</style>
